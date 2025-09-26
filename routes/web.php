bfe69aeabe4bda50395c5861830858b3211e43f0
LAB321 Student13 <giorgi.abshilava@gau.edu.ge>
2025-09-26 11:52:15 +0400

    2nd week

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/home', function () {
+//     return view('home');
+// });
+
+Route::view('/home', 'home', ['name'=>"tako"]);
+
+Route::get('/test1', function () {
+    return "Hello Laravel!";
+});
+
+Route::get('/user/{id?}', function (?string $id=null) {
+    return 'User '.$id;
+});
